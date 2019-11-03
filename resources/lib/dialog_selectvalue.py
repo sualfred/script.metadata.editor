b0b5f0cb94ae371061190b146f3bf1c2db0a980e
Sebastian S <su4lfred@gmail.com>
2019-11-03 10:19:14 +0100

    improved tag+genre dialog

--- resources/lib/dialog_selectvalue.py
+++ resources/lib/dialog_selectvalue.py
@@ -14,17 +14,38 @@ class SelectValue(object):
         self.dbtype = params.get('type')
         self.dbkey = params.get('key')
 
-        self.method_details = 'VideoLibrary.Get%sDetails' % self.dbtype
-        self.method_setdetails = 'VideoLibrary.Set%sDetails' % self.dbtype
+        if self.dbtype in ['movie', 'tvshow', 'season', 'episode', 'musicvideo']:
+            self.library = 'Video'
+            self.nfo_support = True
+        else:
+            self.library = 'Audio'
+            self.nfo_support = False
+
+        self.method_details = '%sLibrary.Get%sDetails' % (self.library, self.dbtype)
+        self.method_setdetails = '%sLibrary.Set%sDetails' % (self.library, self.dbtype)
         self.param = '%sid' % self.dbtype
         self.key_details = '%sdetails' % self.dbtype
         self.dbkey_details = '%ss' % self.dbkey
 
+        self.all_values = []
+        self.duplicate_handler = []
+
         self.init()
 
     def init(self):
+        if self.dbkey == 'genre':
+            if self.dbtype in ['movie', 'tvshow', 'season', 'episode']:
+                self.available_video_values()
+            else:
+                self.available_audio_values()
+
+        else:
+            if self.dbtype in ['movie', 'tvshow', 'season', 'episode', 'musicvideo']:
+                self.available_video_values()
+            else:
+                self.available_audio_values()
+
         self.current_values()
-        self.available_values()
         self.select_dialog()
         self.update_data()
 
@@ -39,38 +60,24 @@ class SelectValue(object):
         self.values = result.get(self.dbkey, [])
         self.file = result.get('file')
 
-    def available_values(self):
-        self.all_values = []
-        duplicate_handler = []
+        # also show musicvideo values if not listed for audio returns
+        if self.dbtype == 'musicvideo' and self.dbkey == 'genre':
+            for item in self.values:
+                if item not in self.all_values:
+                    self.all_values.append(item)
 
-        movies = json_call('VideoLibrary.Get%ss' % self.dbkey,
-                           properties=['title'],
-                           params={'type': 'movie'}
-                           )
+    def available_video_values(self):
+        self._json_query(type='movie')
+        self._json_query(type='tvshow')
 
-        tvshows = json_call('VideoLibrary.Get%ss' % self.dbkey,
-                            properties=['title'],
-                            params={'type': 'tvshow'}
-                            )
+        if self.dbkey != 'genre':
+            self._json_query(type='musicvideo')
 
-        try:
-            for item in movies['result'][self.dbkey_details]:
-                label = item['label']
-                self.all_values.append(label)
-                duplicate_handler.append(label)
+    def available_audio_values(self):
+        self._json_query(library='Audio')
 
-        except KeyError:
-            pass
-
-        try:
-            for item in tvshows['result'][self.dbkey_details]:
-                label = item['label']
-                if label not in duplicate_handler:
-                    self.all_values.append(label)
-                    duplicate_handler.append(label)
-
-        except KeyError:
-            pass
+        if self.dbkey == 'genre':
+            self._json_query(library='Video', type='musicvideo')
 
     def select_dialog(self):
         preselectlist = []
@@ -95,5 +102,29 @@ class SelectValue(object):
                   params={self.param: int(self.dbid), self.dbkey: self.modified}
                   )
 
-        if self.file:
+        if self.nfo_support and self.file:
             write_nfo(self.file, self.dbkey, self.modified, self.dbtype)
+
+    def _json_query(self,library=None,type=None):
+        if library is None:
+            library = self.library
+
+        if type is not None:
+            params = {'type': type}
+        else:
+            params = None
+
+        result = json_call('%sLibrary.Get%ss' % (library, self.dbkey),
+                           properties=['title'],
+                           params=params
+                           )
+
+        try:
+            for item in result['result'][self.dbkey_details]:
+                label = item['label']
+                if label not in self.duplicate_handler:
+                    self.all_values.append(label)
+                    self.duplicate_handler.append(label)
+
+        except KeyError:
+            pass
